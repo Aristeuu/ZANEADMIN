87595f7fb2e21df564b7e9e531262c1a1803c8a5
unknown <aristeuwalker@gmail.com>
2022-06-17 22:23:52 +0100

    up

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\softdeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Banner extends Model
 {
     use HasFactory;
-    use softdeletes;
+    use SoftDeletes;
 
     protected $table = 'banners';
 
