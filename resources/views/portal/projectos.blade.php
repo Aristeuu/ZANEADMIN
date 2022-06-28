71bec9be8fcecd2a0a7d9899e82f97d2fc98de3b
unknown <aristeuwalker@gmail.com>
2022-06-28 18:40:42 +0100

    git

--- resources/views/portal/projectos.blade.php
+++ resources/views/portal/projectos.blade.php
@@ -10,7 +10,7 @@
                 <ol class="breadcrumb text-uppercase mb-0">
                     <li class="breadcrumb-item"><a class="text-white" href="#">PÁGINA INICIAL</a></li>
                     <li class="breadcrumb-item"><a class="text-white" href="#">PÁGINAS</a></li>
-                    <li class="breadcrumb-item text-primary active" aria-current="page">SOBRE NÓS</li>
+                    <li class="breadcrumb-item text-primary active" aria-current="page">Projectos</li>
                 </ol>
             </nav>
         </div>
