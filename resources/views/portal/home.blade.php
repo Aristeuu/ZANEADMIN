8b4251e6aed13fc0e03244f47fa1b5ab63770812
unknown <aristeuwalker@gmail.com>
2022-06-28 21:00:50 +0100

    scontrolls

--- resources/views/portal/home.blade.php
+++ resources/views/portal/home.blade.php
@@ -184,18 +184,25 @@
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                 <h4 class="section-title">Nossos Projectos</h4>
                 <h1 class="display-5 mb-4">Visite os nossos últimos projetos e os nossos trabalhos inovadores</h1>
-            </div>
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            </div>            
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     @foreach ($projectos as $projecto)
                     <div class="carousel-item active">
                         <img src="/public/images/{{$projecto->foto}}" class="d-block w-70" alt="...">
                       </div>
-                    @endforeach
-                
-                                   
+                    @endforeach             
+                  
                 </div>
-              </div>           
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>          
         </div>
     </div>
     <!-- Project End -->
